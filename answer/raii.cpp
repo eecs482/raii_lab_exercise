3ad7b8838494b383b1d3289d1d2a385490e86ae6
Advay Muchoor <advaym@umich.edu>
2023-01-25 12:42:23 -0500

    Rename raii_lock to lock_guard

--- answer/raii.cpp
+++ answer/raii.cpp
@@ -9,14 +9,14 @@ cv cv1;
 
 int child_done = 0;		// global variable; shared between the two threads
 
-class raii_lock {
+class lock_guard {
 public:
   // acquire resources in constructor
-  raii_lock(mutex &in) : my_mutex(in) {
+  lock_guard(mutex &in) : my_mutex(in) {
     my_mutex.lock();
   }
-  // release resources in constructor
-  ~raii_lock() { 
+  // release resources in destructor
+  ~lock_guard() { 
     my_mutex.unlock();
   }
 private:
@@ -27,7 +27,7 @@ void child(void *a)
 {
     auto message = static_cast<char *>(a);
 
-    raii_lock lock(mutex1);
+    lock_guard lock(mutex1);
     cout << "child called with message " << message << ", setting child_done = 1" << endl;
     child_done = 1;
     cv1.signal();
@@ -38,14 +38,14 @@ void parent(void *a)
     auto arg = reinterpret_cast<intptr_t>(a);
 
     {
-        raii_lock lock(mutex1);
+        lock_guard lock(mutex1);
         cout << "parent called with arg " << arg << endl;
     }
 
     thread t1 (reinterpret_cast<thread_startfunc_t>(child), 
                 const_cast<void *>(static_cast<const void *>("test message")));
 
-    raii_lock lock(mutex1);
+    lock_guard lock(mutex1);
     while (!child_done) {
         cout << "parent waiting for child to run\n";
         cv1.wait(mutex1);
